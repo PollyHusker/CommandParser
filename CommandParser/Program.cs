1f9a1e81d2151f2e8d18d4170b19802d3535509f
PollyHusker <baidenko.polina@gmail.com>
2015-09-06 00:39:05 +0300

    version
    version of commandParser util

---
+++ CommandParser/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CommandParser
+{
+    public class CommandParser
+    {
+        static List<string> Tags = new List<string>() { "help", "k", "ping", "print" };
+        static void Main(string[] args)
+        {
+
+            //test
+            TestFunc();
+            Console.ReadLine();
+            return;
+            
+
+            Console.WriteLine("I wish to be a part of study group");
+            if (args != null)
+            {
+                if (!HasWrongTags(args))
+                    ProcessCmd(args);
+            }
+            while (true)
+            {
+                var line = Console.ReadLine();
+                string[] cmd = null;
+                try
+                {
+                    cmd = ToCmdParams(line);
+                    if (!HasWrongTags(cmd))
+                        ProcessCmd(cmd);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+            
+        }
+
+        public static string[] ToCmdParams(string line)
+        {
+            string[] cmdParams = new string[0];
+            List<string> list = new List<string>();
+            line = line.Trim();
+            var li = 0; // line index
+            while(li < line.Length)
+            {
+                try {
+                    while (line[li] == ' ')
+                        li++;
+                    if (line[li] == '\"')
+                    {
+                        var le = line.IndexOf('\"', li + 1);
+                        if (le == -1)
+                            throw new Exception();
+                        list.Add(line.Substring(li, le - li+1)); 
+                        li = le + 1;
+                    }
+                    else
+                    {
+                        var le = line.IndexOf(' ', li + 1);
+                        if (le == -1)
+                            le = line.Length;
+                        list.Add(line.Substring(li, le - li));
+                        li = le + 1;
+                    }
+                }
+                catch(Exception ex)
+                { throw new ArgumentException("Unexpected arguments. Use /? to see set of allowed commands and arguments"); }
+            }
+            return list.ToArray();
+        }
+
+        public static bool HasWrongTags(string[] cmdParams)
+        {
+            foreach (var p in cmdParams)
+            {
+                if (p.StartsWith("-"))
+                {
+                    if (!Tags.Contains(p.Substring(1)))
+                    {
+                        Console.WriteLine(String.Format("Command \"{0}\" is not supported, use /? to see set of allowed commands", p.Substring(1)));
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+ 
+        public static void ProcessCmd(string[] cmdParams)
+        {
+            bool cmndPresent = false;
+            if (cmdParams.Contains("/?") || cmdParams.Contains("-help") || (cmdParams.Contains("/h")))
+            {
+                CmndPrintHelp();
+                return;
+            }
+            
+            var i = 0;
+            try
+            {
+                while (i < cmdParams.Count())
+                {
+                    if (cmdParams[i].StartsWith("-"))
+                    {
+                        var key = cmdParams[i].Substring(1);
+                        switch (key)
+                        {
+                            case "ping":
+                                CmndPing();
+                                i++;
+                                break;
+                            case "print":
+                                i++;
+                                List<string> pParams = GetCmndArgs(cmdParams, i);
+                                CmndPrintMessage(pParams);
+                                i = i + pParams.Count();
+                                break;
+                            case "k":
+                                i++;
+                                List<string> kParams = GetCmndArgs(cmdParams, i);
+                                CmndK(kParams);
+                                i = i + kParams.Count();
+                                break;
+                        }
+                    }
+                    else throw new ArgumentException("Unexpected arguments. Use /? to see set of allowed commands and arguments");
+
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static List<string> GetCmndArgs(string[] cmndLine, int pos)
+        {
+            List<string> args = new List<string>();
+
+            while (pos < cmndLine.Length && !(cmndLine[pos]).StartsWith("-"))
+            {
+                args.Add(cmndLine[pos]);
+                pos++;
+            }
+            return args;
+        }
+
+        static void CmndPrintHelp()
+        {
+            Console.WriteLine("Commands to use [-k key value] [-ping] [-print message] [-help]");
+            Console.WriteLine("-k key value - to print a table, each row contains key - value");
+            Console.WriteLine("-ping - to beep and print \"Pinging...\"");
+            Console.WriteLine("-print message - to print message");
+            Console.WriteLine("-help /h /? - to print this help message");
+        }
+        static void CmndPing()
+        {
+            Console.Beep(300, 300);
+            Console.WriteLine("Pinging...");
+        }
+        static void CmndPrintMessage(List<string> message)
+        {
+            if (message.Count == 0)
+                throw new ArgumentException("Error: No arguments for command -print. Use /? to see set of allowed commands and arguments");
+            if (message.Count == 1)
+            {
+                Console.WriteLine(message[0].Trim('"'));
+                return;
+            }
+            for (int i = 0; i < message.Count - 1; i++)
+                Console.Write(message[i] + " ");
+            Console.WriteLine(message[message.Count - 1]);
+        }
+        static void CmndK(List<string> list)
+        {
+            if (list.Count == 0)
+                throw new ArgumentException("Error: Incorrect input arguments for command -k");
+            if (list.Count % 2 == 0)
+                for (int i = 0; i < list.Count / 2; i++)
+                    Console.WriteLine(String.Format("{0} - {1}", list[2 * i].Trim('"'), list[2 * i + 1].Trim('"')));
+            else
+            {
+                for (int i = 0; i < list.Count / 2; i++)
+                    Console.WriteLine(String.Format("{0} - {1}", list[2 * i].Trim('"'), list[2 * i + 1].Trim('"')));
+                Console.WriteLine(String.Format("{0} - null", list[list.Count() - 1].Trim('"')));
+            }
+        }
+
+        static void TestFunc()
+        {
+            //Test.StringToCmnParamArrayTest();
+            //Test.CorrectCmndLineTest();
+            Test.ProcTest();
+        }
+    }
+}
---
+++ CommandParser/Test.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommandParser
+{
+    public class Test
+    {
+        public static void StringToCmnParamArrayTest()
+        {
+            var line = " -p   l p";
+            OneStrToArr(line);
+
+            line = "  -help    -h  \" [";
+            OneStrToArr(line);
+            line = "  -help    -h  \" 12 3 \" [";
+            OneStrToArr(line);
+            line = "-k  \"-dc\" ok \"cd\" bad \" \"   -print \"  the message ! \" \"next sentence\"  and another one ";
+            OneStrToArr(line);
+        }
+
+        public static void CorrectCmndLineTest()
+        {
+            var line = "-hepl -h";
+            OneCorrCmndLine(line);
+            line = " /h ";
+            OneCorrCmndLine(line);
+            line = " /? ";
+            OneCorrCmndLine(line);
+            line = " /help ";
+            OneCorrCmndLine(line);
+            line = " -k   jdfkl \"-asdas\" -pl ";
+            OneCorrCmndLine(line);
+            line = " -k  ";
+            OneCorrCmndLine(line);
+            line = " -k  gh ";
+            OneCorrCmndLine(line);
+            line = " -k   -print \"\" ";
+            OneCorrCmndLine(line);
+            line = " -k  sdhg -print \"\" ";
+            OneCorrCmndLine(line);
+            line = " -k   -print \"\" ";
+            OneCorrCmndLine(line);
+        }
+
+        public static void ProcTest()
+        {
+            List<string> list = new List<string>();
+            list.Add(" -help ");
+            list.Add(" -k   jdfkl \"-asdas\" -pl ");
+            list.Add(" -k  gh -h -print");
+            list.Add(" -k   -print \"\" ");
+            list.Add("");
+            list.Add(" -k  \"-dc\" ok \"cd\" bad \" \"   - print \"\" ");
+            list.Add(" -k  \"-dc\" ok \"cd\" bad \" \"   -print \"\" ");
+            list.Add("-print \"  the message ! \" ");
+            list.Add("  \"as \"   -print \"  the message ! \" -ping");
+            list.Add(" -k  1 ok 2 bad \"3 \"   -print \"  the message ! \" \"next sentence\"  and another one ");
+            list.Add(" -k  \"-dc\" ok \"cd\" bad \" \" -ping j  -print \"  the message ! \" \"next sentence\"  and another -ping ");
+            list.Add("-ping -print hello everybody! -ping");
+            list.Add("-k \"half life 3\" \"hl 3\"");
+            foreach (var line in list)
+            {
+                {
+                    string[] cmd = null;
+                    Console.WriteLine(line);
+                    try
+                    {
+                        cmd = CommandParser.ToCmdParams(line);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    if (!CommandParser.HasWrongTags(cmd))
+                        CommandParser.ProcessCmd(cmd);
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        private static void OneProcLine(string line)
+        {
+            
+        }
+        private static void OneCorrCmndLine(string line)
+        {
+            try
+            {
+                Console.WriteLine(line);
+                var cmnd = CommandParser.ToCmdParams(line);
+                CommandParser.HasWrongTags(cmnd);
+            }
+            catch (ArgumentException ex)
+            { Console.WriteLine(ex.Message); }
+        }
+
+        private static void OneStrToArr(string line)
+        {
+            Console.WriteLine(line);
+            try
+            {
+                var ar = CommandParser.ToCmdParams(line);
+                PrintArr(ar);
+            }
+            catch (ArgumentException ex)
+            { Console.WriteLine(ex.Message); }
+        }
+        public static void PrintArr(string[] arr)
+        {
+            foreach (var el in arr)
+                Console.WriteLine(el);
+            Console.WriteLine();
+        }
+    }
+}
